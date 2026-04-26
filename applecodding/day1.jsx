f8810dd6ed6204d889239f700df565c5bdd9af29
kwakbookair <ksh@kwakbookairui-MacBookAir.local>
2026-04-26 21:46:50 +0900

    readme 공부한 내용 정리

--- applecodding/day1.jsx
+++ applecodding/day1.jsx
@@ -6,10 +6,6 @@ function App() {
   
   let post = '강남 우동 맛집';
   
-  //JSX문법 1 : Class 넣을 땐 className
-  //JSX문법 2 : 변수값을 사용항 때는 {} 중괄호 사용
-  //원래는 이렇게 사용함 document.querySelector('h4').innerHTML = post;
-  //JSX문법 3 : style 정보 입력할 때 style={{이름 : '값'}}
   return (
     <div className="App">
       <div className="black-nav">
