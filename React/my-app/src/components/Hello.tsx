753436449fc2d801e362f5c0d80e147712cf2d8b
kwakbookair <ksh@kwakbookairui-MacBookAir.local>
2026-05-01 17:51:00 +0900

    리액트 기본 실행환경 구성_20260501

---
+++ React/my-app/src/components/Hello.tsx
@@ -0,0 +1,19 @@
+//Hello는 클릭하면 얼럿을 나타내는 텍스트를 반환한다
+const Hello = () =>{
+    //클릭 시 호출되는 함수
+    const onClick = () =>{
+        alert('hello');
+    }
+
+    const text = 'Hello React';
+
+    //텍스트를 자식으로 갖는 div요소를 반환한다.
+    return (
+        //div의 onClick에 클릭 시의 콜백 함수를 반환한다
+        <div onClick={onClick}>
+            {text}
+        </div>
+    );
+}
+//외부로부터 Hello를 읽을 수 있게 export처리를 한다
+export default Hello
--- applecodding/day1.jsx
+++ applecodding/day1.jsx
@@ -7,11 +7,20 @@ function App() {
   let post = '강남 우동 맛집';
   
   let [글제목,b] = useState('남자 코트 추천');  //State문법
+  let [글제목,b] = useState(['남자 코트 추천','강남 우동맛집', '파이썬독학']); //배열로 처리가능
     
   return (
     <div className="App">
       <div className="black-nav">
         <h4 style={{color: 'red', fontSize : '16px'}}>블로그임</h4>
+      </div>
+      <div className="list">
+        <h4>{ 글제목 }</h4>
+        <p>4월 26일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목 }</h4>
+        <p>4월 26일 발행</p>
       </div>
       <div className="list">
         <h4>{ 글제목 }</h4>
